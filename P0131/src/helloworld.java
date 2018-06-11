38c08bc6d7887d579cb35e723a9077ebdee3210a
USER <USER@DESKTOP-ANJMC4L>
2018-06-11 17:28:37 +0900

    aa

---
+++ P0131/src/helloworld.java
@@ -0,0 +1,20 @@
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		double n1;
+		int n2;
+		double result;
+		n1=10.0f;
+		n2=3;
+		result=n1/n2;
+		
+		System.out.print("결과 : "+result);
+		System.out.print("\n***************\n");
+		System.out.println("결과 : "+result);
+		System.out.println("***************");
+		System.out.printf("%.1f + %d = %.1f", n1,n2,result);
+		
+	}
+
+}
